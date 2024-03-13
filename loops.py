4cc3f200c7f97a68951771f0b64073c10345b3ad
Your Name <you@example.com>
2024-03-14 00:09:59 +0530

    commit 2

---
+++ loops.py
@@ -0,0 +1,38 @@
+#while loop
+x = 0
+while x<=10:
+    print(x)
+    x=x+1
+
+print()
+y = 0
+while y<10:
+    y=y+1
+    if y==5:
+       continue
+    print(y)
+    
+print()
+z = 0
+while z<10:
+    print(z)
+    z=z+1
+    if z==5:
+       break
+
+
+print()
+
+
+n = 0
+#for loop
+for n in "university":
+    print(n)
+
+print()
+for n in range(8):
+    print(n)
+
+print()
+for n in range(1,8,3):
+    print(n)
